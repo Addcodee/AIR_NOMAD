85fca3b36c071f6257a8c7c56de9bcd742c88d06
Alibek <musicchannel0p@gmail.com>
2023-02-09 00:16:42 +0600

    made half navbar

---
+++ src/Routes/MainRoutes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import CreateHomePage from "../Pages/CreateHomePage/CreateHomePage";
+import HomePage from "../Pages/HomePage/HomePage";
+import LoginPage from "../Pages/LoginPage/LoginPage";
+import NotFoundPage from "../Pages/NotFoundPage/NotFoundPage";
+import RegisterPage from "../Pages/RegisterPage/RegisterPage";
+
+const MainRoutes = () => {
+  const PUBLIC_ROUTES = [
+    { path: "/", element: <HomePage />, id: 1 },
+    { path: "/register", element: <RegisterPage />, id: 2 },
+    { path: "/login", element: <LoginPage />, id: 3 },
+    { path: "/homes", element: <CreateHomePage />, id: 4 },
+    { path: "*", element: <NotFoundPage />, id: 5 },
+  ];
+  return (
+    <>
+      <Routes>
+        {PUBLIC_ROUTES.map((obj) => (
+          <Route path={obj.path} element={obj.element} key={obj.id} />
+        ))}
+      </Routes>
+    </>
+  );
+};
+
+export default MainRoutes;
